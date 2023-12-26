3fdfaeb289888ee2f6bc95f1c6d36048c16de1b6
Umaamah Leghari <uleghari.bese21seecs@student.nust.edu.pk>
2023-12-26 21:19:16 +0500

    Added Categories view

---
+++ sustainable-pak/resources/views/Main/categories.blade.php
@@ -0,0 +1,18 @@
+
+@extends('master_view')
+
+@section('section')
+
+@foreach($categories->slice(0, 3) as $category)
+    <div class="individual-element-container">
+        <a href="{{ route('business.list', ['id' => $category->id]) }}">
+            <img src="{{ asset($category->img_link) }}" alt="{{ $category->name }}">
+        </a>
+        <a href="{{ route('business.list', ['id' => $category->id]) }}">
+            {{ $category->name }}
+        </a>
+    </div>
+@endforeach
+
+        
+@endsection
